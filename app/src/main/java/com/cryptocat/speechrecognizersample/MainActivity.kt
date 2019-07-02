f70a79d496e5982cc908196723e48045dfc829c4
cryptocat-miner <43912836+cryptocat-miner@users.noreply.github.com>
2019-07-02 13:47:03 +0900

    Androidプロジェクト作成

---
+++ app/src/main/java/com/cryptocat/speechrecognizersample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cryptocat.speechrecognizersample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
